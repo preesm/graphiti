f4fca704f8f45c57a813da28734ba166a0e94e2a
Matthieu Wipliez <matthieu.wipliez@insa-rennes.fr>
2008-10-24 17:57:12 +0000

    Added a hook for a Convert action. Changed dependency icon.
    git-svn-id: http://svn.code.sf.net/p/graphiti-editor/code@231 04dbf5ff-8551-0410-9f1f-ce2c5325ca72

--- plugin/src/java/net/sf/graphiti/ui/editors/GraphActionBarContributor.java
+++ plugin/src/java/net/sf/graphiti/ui/editors/GraphActionBarContributor.java
@@ -28,6 +28,9 @@
  */
 package net.sf.graphiti.ui.editors;
 
+import net.sf.graphiti.ui.actions.ConvertAction;
+import net.sf.graphiti.ui.actions.ConvertRetargetAction;
+
 import org.eclipse.gef.ui.actions.ActionBarContributor;
 import org.eclipse.gef.ui.actions.GEFActionConstants;
 import org.eclipse.gef.ui.actions.ZoomComboContributionItem;
@@ -62,12 +65,17 @@ public class GraphActionBarContributor extends ActionBarContributor {
 		addRetargetAction((RetargetAction) ActionFactory.REDO.create(iww));
 		addRetargetAction((RetargetAction) ActionFactory.UNDO.create(iww));
 
+		addRetargetAction(new ConvertRetargetAction());
 		addRetargetAction(new ZoomInRetargetAction());
 		addRetargetAction(new ZoomOutRetargetAction());
 	}
 
 	@Override
 	public void contributeToToolBar(IToolBarManager toolBarManager) {
+		toolBarManager.add(getAction(ConvertAction.getActionId()));
+		
+		toolBarManager.add(new Separator());
+		
 		toolBarManager.add(getAction(ActionFactory.UNDO.getId()));
 		toolBarManager.add(getAction(ActionFactory.REDO.getId()));
 		toolBarManager.add(getAction(ActionFactory.DELETE.getId()));
