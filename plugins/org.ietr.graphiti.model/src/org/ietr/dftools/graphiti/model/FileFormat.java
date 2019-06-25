c50458cda6c8b5f0506200946cbec4c2947adf4e
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-06-25 14:03:43 +0200

    [RELENG] Fix headers

--- plugins/org.ietr.graphiti.model/src/org/ietr/dftools/graphiti/model/FileFormat.java
+++ plugins/org.ietr.graphiti.model/src/org/ietr/dftools/graphiti/model/FileFormat.java
@@ -1,9 +1,9 @@
 /**
- * Copyright or © or Copr. IETR/INSA - Rennes (2009 - 2018) :
+ * Copyright or © or Copr. IETR/INSA - Rennes (2009 - 2019) :
  *
- * Antoine Morvan <antoine.morvan@insa-rennes.fr> (2017 - 2018)
- * Clément Guy <clement.guy@insa-rennes.fr> (2014 - 2015)
- * Matthieu Wipliez <matthieu.wipliez@insa-rennes.fr> (2009 - 2011)
+ * Antoine Morvan [antoine.morvan@insa-rennes.fr] (2017 - 2019)
+ * Clément Guy [clement.guy@insa-rennes.fr] (2014 - 2015)
+ * Matthieu Wipliez [matthieu.wipliez@insa-rennes.fr] (2009 - 2011)
  *
  * This software is a computer program whose purpose is to help prototyping
  * parallel applications using dataflow formalism.
